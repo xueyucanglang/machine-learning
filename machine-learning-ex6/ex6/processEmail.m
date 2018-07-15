8a7e756a0d16d7fc4196038c69f6db1d00453d03
xueyucanglang <beihuatianyue@163.com>
2018-07-16 06:09:46 +0800

    vectorize ,corresponding location Word list set 1

--- machine-learning-ex6/ex6/processEmail.m
+++ machine-learning-ex6/ex6/processEmail.m
@@ -96,8 +96,13 @@ while ~isempty(email_contents)
     % Note: You can use strcmp(str1, str2) to compare two strings (str1 and
     %       str2). It will return 1 only if the two strings are equivalent.
     %
-
-
+L1=length(vocabList)
+for i=1:L1
+  if(strcmp(str,vocabList{i})==1)
+  word_indices=[word_indices;i];
+endif
+i++;
+endfor
 
 
 
