c949c6a11fb72b5a1e5d512fa8cbee85d7be3e00
xueyucanglang <beihuatianyue@163.com>
2018-07-15 18:02:36 +0800

    compelte choose C&&sigma ,use every combination C&sigma to train model

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -11,6 +11,7 @@ function [C, sigma] = dataset3Params(X, y, Xval, yval)
 C = 1;
 sigma = 0.3;
 
+
 % ====================== YOUR CODE HERE ======================
 % Instructions: Fill in this function to return the optimal C and sigma
 %               learning parameters found using the cross validation set.
@@ -23,6 +24,20 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+C1 = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30]';
+sigma1 = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30]';
+error1=zeros(8,8);
+pred1=zeros(length(y));
+for i=1:8
+  for j=1:8
+    model= svmTrain(X, y, C1(i), @(x1, x2) gaussianKernel(x1, x2, sigma(j))); %%try every combination of C&sigma in train set
+    pred1 = svmPredict(model, Xval);%%use the model and cross validation set to predict yval
+    error1(i,j)=mean(double(pred1~=yval));%% calculate the error between prediction and real yval
+    
+  endfor
+endfor
+[indc,indr]=find(error1==min(min(error1)));%%find the index of minus error
+C=C1(indc);sigma=sigma1(indr);%% use the index to find optimal C&sigma
 
 
 
